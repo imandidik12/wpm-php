32bf2585535a43ff7c94a07ac975b27375c5d011
imandidik12 <imandidikr@gmail.com>
2019-11-26 08:36:34 +0700

    Change algorithm Using S vector & V vector score

--- test.php
+++ test.php
@@ -19,5 +19,86 @@ $data = [
     'A3'=>$dataset[2],
 ];
 $cost = [2,5];
-$rumus = new Rumus($data, $wr, $cost);
-$rumus->get_formatted();
+$wpm = new Rumus($data, $wr, $cost);
+$res = $wpm->get_formatted();
+?>
+
+<h1>Raw</h1>
+
+<?php
+
+echo '<table>';
+$th = ['Alternatives','C1','C2','C3','C4','C5'];
+echo '<thead><tr>';
+foreach ($th as $_th){
+    echo "<th>$_th</th>";
+}
+echo '</tr></thead>';
+echo '<tbody>';
+foreach ($res as $index=> $alternative){
+    echo '<tr>';
+    echo "<th>$index</th>";
+    foreach ($alternative['criterias'] as $criteria){
+        echo "<td>$criteria</td>";
+    }
+    echo '</tr>';
+}
+echo '</tbody>';
+echo '</table>';
+?>
+
+    <h1>Normalized</h1>
+
+<?php
+echo 'Weight = [';
+foreach ($wr as $_preference) {
+    echo $_preference . ',';
+}
+echo ']<br>';
+echo 'Normalized Weight = [';
+$wpm->preferences->normalized->each(function ($entry){
+    echo round($entry,2).',';
+});
+echo ']';
+echo '<table>';
+$th = ['Alternatives','C1','C2','C3','C4','C5'];
+echo '<thead><tr>';
+foreach ($th as $_th){
+    echo "<th>$_th</th>";
+}
+echo '</tr></thead>';
+echo '<tbody>';
+foreach ($res as $index=> $alternative){
+    echo '<tr>';
+    echo "<th>$index</th>";
+    foreach ($alternative['normalized'] as $criteria){
+        echo "<td>$criteria</td>";
+    }
+    echo '</tr>';
+}
+echo '</tbody>';
+echo '</table>';
+?>
+
+    <h1>Scores</h1>
+
+<?php
+
+echo '<table>';
+$th = ['Alternatives','Normalized scores','Scores'];
+echo '<thead><tr>';
+foreach ($th as $_th){
+    echo "<th>$_th</th>";
+}
+echo '</tr></thead>';
+echo '<tbody>';
+foreach ($res as $index=> $alternative){
+    echo '<tr>';
+    echo "<th>$index</th>";
+    foreach ($alternative['score'] as $score){
+        echo "<td>$score</td>";
+    }
+    echo '</tr>';
+}
+echo '</tbody>';
+echo '</table>';
